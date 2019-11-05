ee2685b143622d50a4cfa8a3d57ab5e69c264158
Chris <ctd2466@rit.edu>
2019-11-05 10:52:34 -0500

    this one

--- project2-1-faizan1124/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module Project2 {
-    // the GUI uses javaFX
-    requires transitive javafx.controls;
-    // lib/Pathbuilder.jar is the provided framework, an obfuscated jar file
-    requires Pathbuilder;
-}
